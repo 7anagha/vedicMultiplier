4414fadb548e77eeaf27cd00e8b085d8e16bd9b9
olutuleoatiq <olutuleoatiq@13.57.40.178>
2019-02-18 23:16:38 +0000

    updated the floorplan wit power commands

--- floorplan.tcl
+++ floorplan.tcl
@@ -6,3 +6,10 @@ set_floorplan_parameters -WIDTH 500 -HEIGHT 500
 set_floorplan -force -partition mult2
 write_def -output mult2.def --overwrite
 defIn -def mult.def --all
+
+create_net -type power -name VDD
+create_net -type ground -name VSS
+
+addPowerRing -offset {0,0} -spacing 1 -width 1 -layerH metal7 -layerV metal8 -nets {VDD,VSS}
+addHardMacroPowerRing -offset {1,1} -spacing 1 -width 1 -layerH metal7 -layerV metal8 -nets {VDD,VSS} -instance U1
+addHardMacroPowerRing -offset {1,1} -spacing 1 -width 1 -layerH metal7 -layerV metal8 -nets {VDD,VSS} -instance U3
